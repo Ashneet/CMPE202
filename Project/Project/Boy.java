9b51f287030567d805cd79943346d3c0af9219a0
Manish Belsare <manishbelsare2003@gmail.com>
2013-10-12 15:25:28 -0700

    World created

---
+++ Project/Project/Boy.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Boy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Boy extends Actor
+{
+    /**
+     * Act - do whatever the Boy wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-3);
+        }
+        
+         if(Greenfoot.isKeyDown("right"))
+        {
+            move(3);
+        }
+    }    
+}
