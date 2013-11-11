0fd861fd8f2b0770b10f8a2902daecc4032bbf95
Manish Belsare <manishbelsare2003@gmail.com>
2013-11-10 18:29:40 -0800

    Changed the Pointer class

--- Project/Pointer.java
+++ Project/Pointer.java
@@ -1,32 +1,42 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-import java.util.List;
-
-/**
- * Write a description of class Poiner here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Pointer extends GamePlay
-{
+    import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+    import java.util.List;
+    
     /**
-     * Act - do whatever the Poiner wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Write a description of class Poiner here.
+     * 
+     * @author (your name) 
+     * @version (a version number or a date)
      */
-    
-    public void act() 
+    public class Pointer extends GamePlay
     {
-        setRotation(270);
-         if(Greenfoot.isKeyDown("z"))
-        {
-            this.move(3);
-            if(this.getY() < 10)
-            {
-                this.setLocation(25, 500);
-            }
-        }
         
-    
-    }    
-   
-}
+       int speed = 3;  
+       
+        /**
+         * Act - do whatever the Poiner wants to do. This method is called whenever
+         * the 'Act' or 'Run' button gets pressed in the environment.
+         */
+        
+        
+        
+        public void act() 
+        {
+            setRotation(270);
+           
+             if(Greenfoot.isKeyDown("z"))
+            {
+                this.move(speed);
+                if(speed < 10)
+                {
+                speed++;
+            
+            }
+                if(this.getY() < 10)
+                {
+                    this.setLocation(25, 500);
+                }
+            }
+            
+        }    
+       
+    }
